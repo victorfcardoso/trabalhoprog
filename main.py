f335fc4f80eeb0b3d3659839f00112415a22c049
victor cardoso <victorfigueiracardoso@gmail.com>
2024-05-07 10:36:34 -0300

    handling for when the discriminant is zero or negative

--- main.py
+++ main.py
@@ -3,8 +3,16 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 def solveQuadraticEquation(a, b, c):
-    raiz1 = (-b + (b**2 - 4 * a * c)**(1/2)) / (2 * a)
-    raiz2 = (-b - (b**2 - 4 * a * c)**(1/2)) / (2 * a)
-    print(raiz1, raiz2)
-    
+    discriminant = b**2 - 4 * a * c
+    if discriminant > 0:
+        raiz1 = (-b + (b**2 - 4 * a * c)**(1/2)) / (2 * a)
+        raiz2 = (-b - (b**2 - 4 * a * c)**(1/2)) / (2 * a)
+    elif discriminant == 0:
+        raiz1 = (-b + (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
+        raiz2 = "NA"
+    else:
+        raiz1 = "NA"
+        raiz2 = "NA"
+    return raiz1, raiz2
+
 solveQuadraticEquation(1, -3, 2)
