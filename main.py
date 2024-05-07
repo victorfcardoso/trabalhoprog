744f9a884216114eadb97a9dd72e02657ec85a0f
victor cardoso <victorfigueiracardoso@gmail.com>
2024-05-07 10:48:40 -0300

    function now accepts lists of coefficients as parametes

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 def solveQuadraticEquation(a, b, c):
     discriminant = b**2 - 4 * a * c
     if discriminant > 0:
-        raiz1 = (-b + (b**2 - 4 * a * c)**(1/2)) / (2 * a)
-        raiz2 = (-b - (b**2 - 4 * a * c)**(1/2)) / (2 * a)
+        raiz1 = (-b + (b ** 2 - 4 * a * c)**(1/2)) / (2 * a)
+        raiz2 = (-b - (b ** 2 - 4 * a * c)**(1/2)) / (2 * a)
     elif discriminant == 0:
         raiz1 = (-b + (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
         raiz2 = "NA"
@@ -15,4 +15,14 @@ def solveQuadraticEquation(a, b, c):
         raiz2 = "NA"
     return raiz1, raiz2
 
-solveQuadraticEquation(1, -3, 2)
+def solve_quadratics(a, b , c):
+    raizes = []
+    for i in range(len(a)):
+        raizes.append(solveQuadraticEquation(a[i], b[i], c[i]))
+    return raizes
+
+a = [2, 1, 3]
+b = [-3, -6, -2]
+c = [-2, 9, 1]
+
+print(solve_quadratics(a, b, c))
