69a14b602224317e5bf76501c98bf2c1bf2ff38b
victor cardoso <victorfigueiracardoso@gmail.com>
2024-06-11 20:39:58 -0300

    Added Docstrings and code comments

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
 from typing import List, Union
 
 def solve_single_quadratic(a: float, b: float, c: float) -> List[Union[float, str]]:
+    """
+    Calculates the root of a single quadratic equation of the form a*x^2 + b*x + c = 0
+    :param a: coefficient of x^2
+    :param b: coefficient of x
+    :param c: constant
+    :return: A list (in decreasing order) of the roots of the equation. If there's no root, the function returns 'NA'
+    """
     try:
+        # Calculate the discriminant
         discriminant = b**2 - 4 * a * c
         if discriminant > 0:
+            # For the case of two distinct real roots
             raiz1 = (-b - (b ** 2 - 4 * a * c)**(1/2)) / (2 * a)
             raiz2 = (-b + (b ** 2 - 4 * a * c)**(1/2)) / (2 * a)
         elif discriminant == 0:
+            # One real root
             raiz1 = (-b + (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
             raiz2 = "NA"
         else:
+            # No real roots
             raiz1 = "NA"
             raiz2 = "NA"
-        return raiz1, raiz2
+        return [raiz1, raiz2]
     except Exception as e:
+        # In case of error, return an error message
         return [f"Error: {e}"]
 
 
 def solve_quadratics(a: List[float], b: List[float], c: List[float]) -> List[List[Union[float , str ]]]:
+    """
+    Calculates the root of multiples quadratics equations of the form a*x^2 + b*x + c = 0
+    :param a: List of coefficient for x^2
+    :param b: List of coefficient for x
+    :param c: List of constant terms
+    :return: A list of lists containing the roots (in decreasing order) of each quadratic equation. If there's no root, the function returns 'NA'
+    """
+    # Check if all lists have the same length
     if len(a) != len(b) or len(a) != len(c) or len(b) != len(c):
         return [["Error: All input lists must have the same length"]]
 
     raizes = []
+    # Use a loop in order to solve for each quadratic equation
     for i in range(len(a)):
         try:
             raizes.append(solve_single_quadratic(a[i], b[i], c[i]))
         except Exception as e:
+            # Append an error message in case of error
             raizes.append([f"Error: {e}"])
     return raizes
 
