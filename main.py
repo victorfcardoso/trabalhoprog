79e319c89830395732e4b8e294f6abf07b38e282
victor cardoso <victorfigueiracardoso@gmail.com>
2024-06-11 19:29:38 -0300

    Implemented exception handling

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from typing import List, Union
 
 def solve_single_quadratic(a: float, b: float, c: float) -> List[Union[float, str]]:
-    discriminant = b**2 - 4 * a * c
-    if discriminant > 0:
-        raiz1 = (-b + (b ** 2 - 4 * a * c)**(1/2)) / (2 * a)
-        raiz2 = (-b - (b ** 2 - 4 * a * c)**(1/2)) / (2 * a)
-    elif discriminant == 0:
-        raiz1 = (-b + (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
-        raiz2 = "NA"
-    else:
-        raiz1 = "NA"
-        raiz2 = "NA"
-    return raiz1, raiz2
+    try:
+        discriminant = b**2 - 4 * a * c
+        if discriminant > 0:
+            raiz1 = (-b + (b ** 2 - 4 * a * c)**(1/2)) / (2 * a)
+            raiz2 = (-b - (b ** 2 - 4 * a * c)**(1/2)) / (2 * a)
+        elif discriminant == 0:
+            raiz1 = (-b + (b ** 2 - 4 * a * c) ** (1 / 2)) / (2 * a)
+            raiz2 = "NA"
+        else:
+            raiz1 = "NA"
+            raiz2 = "NA"
+        return raiz1, raiz2
+    except Exception as e:
+        return [f"Error: {e}"]
+
 
 def solve_quadratics(a: List[float], b: List[float], c: List[float]) -> List[List[Union[float , str ]]]:
     raizes = []
-    for i in range(len(a)):
-        raizes.append(solve_single_quadratic(a[i], b[i], c[i]))
+    try:
+        for i in range(len(a)):
+            raizes.append(solve_single_quadratic(a[i], b[i], c[i]))
+    except Exception as e:
+        solutions.append([f"Error: {e}"])
     return raizes
 
 a = [2, 1, 3]
