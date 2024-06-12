bc85b7e5af3b51885a43cf677c86d1d217baa5d3
victor cardoso <victorfigueiracardoso@gmail.com>
2024-06-11 21:09:08 -0300

    Improved commentaries and removed example usage

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from typing import List, Union
 def solve_single_quadratic(a: float, b: float, c: float) -> List[Union[float, str]]:
     """
     Calculates the root of a single quadratic equation of the form a*x^2 + b*x + c = 0
-    :parameter a: coefficient of x^2
-    :parameter b: coefficient of x
-    :parameter c: constant
+    :param a: coefficient of x^2
+    :type a: float
+    :param b: coefficient of x
+    :type b: float
+    :param c: constant
+    :type c: float
     :return: A list (in increasing order) of the roots of the equation. If there's no root, the function returns 'NA'
+    :rtype: List[Union[float, str]]
     """
     try:
         # Calculate the discriminant
@@ -32,27 +36,27 @@ def solve_single_quadratic(a: float, b: float, c: float) -> List[Union[float, st
 def solve_quadratics(a: List[float], b: List[float], c: List[float]) -> List[List[Union[float , str ]]]:
     """
     Calculates the root of multiples quadratics equations of the form a*x^2 + b*x + c = 0
-    :parameter a: List of coefficient for x^2
-    :parameter b: List of coefficient for x
-    :parameter c: List of constant terms
+    :param a: List of coefficient for x^2
+    :type a: List[float]
+    :param b: List of coefficient for x
+    :type b: List[float]
+    :param c: List of constant terms
+    :type c: List[float]
     :return: A list of lists containing the roots (in increasing order) of each quadratic equation. If there's no root, the function returns 'NA'
+    :rtype: List[List[Union[float, str]]]
     """
     # Check if all lists have the same length
     if len(a) != len(b) or len(a) != len(c) or len(b) != len(c):
         return [["Error: All input lists must have the same length"]]
 
     raizes = []
-    # Use a loop in order to solve for each quadratic equation
+    # Use a loop in order iterate over each set of coefficients
     for i in range(len(a)):
         try:
+            # Solve the equation for the current coefficients and append to the raizes list
             raizes.append(solve_single_quadratic(a[i], b[i], c[i]))
         except Exception as e:
             # Append an error message in case of error
             raizes.append([f"Error: {e}"])
     return raizes
 
-a = [2, 1, 3]
-b = [-3, -6, -2]
-c = [-2, 9, 1]
-
-print(solve_quadratics(a, b, c))
